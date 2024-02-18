99dae24bb7dd861ac4850ac98442895480b6e843
TRKQA23 <tanmayqaa2023@gmail.com>
2024-02-18 11:58:07 +0530

    exception  commit

---
+++ src/main/java/ExceptionConcept/ArrayIndexOutOfBound_Exception.java
@@ -0,0 +1,22 @@
+package ExceptionConcept;
+
+public class ArrayIndexOutOfBound_Exception {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*ArrayIndexOutOfBounds Exception : It is thrown to indicate that an array has been accessed with an illegal index. 
+		The index is either negative or greater than or equal to the size of the array.*/
+		
+		try {
+			int a[] = new int[5];
+			a[6] = 9; // accessing 7th element in an array of size 5
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Array Index is Out Of Bounds");
+		}
+
+		
+
+	}
+
+}
